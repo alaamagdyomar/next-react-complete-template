35eb1780aa3ec060f1b30a95dbae6dd7765f1fef
Alaa-Magdy <alaa@tammel.com>
2023-10-30 04:07:17 +0200

    ready template with auth system to be managed

---
+++ src/pages/login/index.js
@@ -0,0 +1,276 @@
+// ** React Imports
+import { useState } from 'react'
+
+// ** Next Imports
+import Link from 'next/link'
+
+// ** MUI Components
+import Alert from '@mui/material/Alert'
+import Button from '@mui/material/Button'
+import Divider from '@mui/material/Divider'
+import Checkbox from '@mui/material/Checkbox'
+import Typography from '@mui/material/Typography'
+import IconButton from '@mui/material/IconButton'
+import Box from '@mui/material/Box'
+import useMediaQuery from '@mui/material/useMediaQuery'
+import { styled, useTheme } from '@mui/material/styles'
+import InputAdornment from '@mui/material/InputAdornment'
+import MuiFormControlLabel from '@mui/material/FormControlLabel'
+
+// ** Custom Component Import
+import CustomTextField from 'src/@core/components/mui/text-field'
+
+// ** Icon Imports
+import Icon from 'src/@core/components/icon'
+import Logo from '../../../public/images/pages/img.png'
+import Image from 'next/image'
+
+// ** Third Party Imports
+import * as yup from 'yup'
+import { useForm, Controller } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+
+// ** Hooks
+import { useAuth } from 'src/hooks/useAuth'
+import useBgColor from 'src/@core/hooks/useBgColor'
+import { useSettings } from 'src/@core/hooks/useSettings'
+
+// ** Configs
+import themeConfig from 'src/configs/themeConfig'
+
+// ** Layout Import
+import BlankLayout from 'src/@core/layouts/BlankLayout'
+
+// ** Demo Imports
+import FooterIllustrationsV2 from 'src/views/pages/auth/FooterIllustrationsV2'
+
+// ** Styled Components
+const LoginIllustration = styled('img')(({ theme }) => ({
+  zIndex: 2,
+  maxHeight: 680,
+  marginTop: theme.spacing(12),
+  marginBottom: theme.spacing(12),
+  [theme.breakpoints.down(1540)]: {
+    maxHeight: 3000
+  },
+  [theme.breakpoints.down('lg')]: {
+    maxHeight: 5000
+  }
+}))
+
+const RightWrapper = styled(Box)(({ theme }) => ({
+  width: '100%',
+  [theme.breakpoints.up('md')]: {
+    maxWidth: 450
+  },
+  [theme.breakpoints.up('lg')]: {
+    maxWidth: 600
+  },
+  [theme.breakpoints.up('xl')]: {
+    maxWidth: 750
+  }
+}))
+
+const LinkStyled = styled(Link)(({ theme }) => ({
+  textDecoration: 'none',
+  color: `${theme.palette.primary.main} !important`
+}))
+
+const FormControlLabel = styled(MuiFormControlLabel)(({ theme }) => ({
+  '& .MuiFormControlLabel-label': {
+    color: theme.palette.text.secondary
+  }
+}))
+
+const schema = yup.object().shape({
+  email: yup.string().email().required(),
+  password: yup.string().min(5).required()
+})
+
+const defaultValues = {
+  password: 'admin',
+  email: 'admin@admin.com'
+}
+
+const LoginPage = () => {
+  const [rememberMe, setRememberMe] = useState(true)
+  const [showPassword, setShowPassword] = useState(false)
+
+  // ** Hooks
+  const auth = useAuth()
+  const theme = useTheme()
+  const bgColors = useBgColor()
+  const { settings } = useSettings()
+  const hidden = useMediaQuery(theme.breakpoints.down('md'))
+
+  // ** Vars
+  const { skin } = settings
+
+  const {
+    control,
+    setError,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({
+    defaultValues,
+    mode: 'onBlur',
+    resolver: yupResolver(schema)
+  })
+
+  const onSubmit = data => {
+    const { email, password } = data
+    auth.login({ email, password, rememberMe }, () => {
+      setError('email', {
+        type: 'manual',
+        message: 'Email or Password is invalid'
+      })
+    })
+  }
+
+  // const imageSource = skin === 'bordered' ? 'img' : 'img'
+
+  return (
+    <div className='login-container'>
+      <div className='left-container'>
+        <p className='first-parag'>WELCOME TO DASHBOARD</p>
+        <p className='second-parag'>
+          Lorem ipsum dolor sit armet , consectetur adipiscing elit , sed do eiusmod tempor
+        </p>
+      </div>
+      <div className='right-container'>
+        <RightWrapper
+          style={{
+            width: '100%'
+          }}
+        >
+          <Box
+            sx={{
+              p: [6, 8],
+              height: '100vh',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center'
+            }}
+          >
+            <Box sx={{ width: '100%', maxWidth: 400 }}>
+              <form noValidate autoComplete='off' onSubmit={handleSubmit(onSubmit)}>
+                <Box sx={{ mb: 4 }}>
+                  <Controller
+                    name='email'
+                    control={control}
+                    rules={{ required: true }}
+                    render={({ field: { value, onChange, onBlur } }) => (
+                      <CustomTextField
+                        fullWidth
+                        autoFocus
+                        label='Email'
+                        value={value}
+                        onBlur={onBlur}
+                        onChange={onChange}
+                        placeholder='admin@vuexy.com'
+                        error={Boolean(errors.email)}
+                        {...(errors.email && { helperText: errors.email.message })}
+                      />
+                    )}
+                  />
+                </Box>
+                <Box sx={{ mb: 1.5 }}>
+                  <Controller
+                    name='password'
+                    control={control}
+                    rules={{ required: true }}
+                    render={({ field: { value, onChange, onBlur } }) => (
+                      <CustomTextField
+                        fullWidth
+                        value={value}
+                        onBlur={onBlur}
+                        label='Password'
+                        onChange={onChange}
+                        id='auth-login-v2-password'
+                        error={Boolean(errors.password)}
+                        {...(errors.password && { helperText: errors.password.message })}
+                        type={showPassword ? 'text' : 'password'}
+                        InputProps={{
+                          endAdornment: (
+                            <InputAdornment position='end'>
+                              <IconButton
+                                edge='end'
+                                onMouseDown={e => e.preventDefault()}
+                                onClick={() => setShowPassword(!showPassword)}
+                              >
+                                <Icon fontSize='1.25rem' icon={showPassword ? 'tabler:eye' : 'tabler:eye-off'} />
+                              </IconButton>
+                            </InputAdornment>
+                          )
+                        }}
+                      />
+                    )}
+                  />
+                </Box>
+                <Box
+                  sx={{
+                    mb: 1.75,
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    alignItems: 'center',
+                    justifyContent: 'space-between'
+                  }}
+                >
+                  <FormControlLabel
+                    label='Remember Me'
+                    control={<Checkbox checked={rememberMe} onChange={e => setRememberMe(e.target.checked)} />}
+                  />
+                  <Typography component={LinkStyled} href='/forgot-password'>
+                    Forgot Password?
+                  </Typography>
+                </Box>
+                <Button fullWidth type='submit' variant='contained' sx={{ mb: 4 }}>
+                  Login
+                </Button>
+                <Box sx={{ display: 'flex', alignItems: 'center', flexWrap: 'wrap', justifyContent: 'center' }}>
+                  <Typography sx={{ color: 'text.secondary', mr: 2 }}>New on our platform?</Typography>
+                  <Typography href='/register' component={LinkStyled}>
+                    Create an account
+                  </Typography>
+                </Box>
+                <Divider
+                  sx={{
+                    color: 'text.disabled',
+                    '& .MuiDivider-wrapper': { px: 6 },
+                    fontSize: theme.typography.body2.fontSize,
+                    my: theme => `${theme.spacing(6)} !important`
+                  }}
+                >
+                  or
+                </Divider>
+                <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                  <IconButton href='/' component={Link} sx={{ color: '#497ce2' }} onClick={e => e.preventDefault()}>
+                    <Icon icon='mdi:facebook' />
+                  </IconButton>
+                  <IconButton href='/' component={Link} sx={{ color: '#1da1f2' }} onClick={e => e.preventDefault()}>
+                    <Icon icon='mdi:twitter' />
+                  </IconButton>
+                  <IconButton
+                    href='/'
+                    component={Link}
+                    onClick={e => e.preventDefault()}
+                    sx={{ color: theme => (theme.palette.mode === 'light' ? '#272727' : 'grey.300') }}
+                  >
+                    <Icon icon='mdi:github' />
+                  </IconButton>
+                  <IconButton href='/' component={Link} sx={{ color: '#db4437' }} onClick={e => e.preventDefault()}>
+                    <Icon icon='mdi:google' />
+                  </IconButton>
+                </Box>
+              </form>
+            </Box>
+          </Box>
+        </RightWrapper>
+      </div>
+    </div>
+  )
+}
+LoginPage.getLayout = page => <BlankLayout>{page}</BlankLayout>
+LoginPage.guestGuard = true
+
+export default LoginPage
